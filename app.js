5225fe33d0062ed2bd0644aa0967b88e0793a06b
AbubakarYasir <abubakaryasir569@gmail.com>
2022-03-04 23:13:26 +0500

    renamed index.js to app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.use("/public", express.static(process.cwd() + "/public"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("lists", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+});
+
+app.listen(port, function () {
+    console.log("Server is started at http://localhost:" + port);
+});
