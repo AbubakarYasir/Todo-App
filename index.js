d5e8637a5622de120e38ef7a574d4675ff203de5
Abubakar Yasir <abubakaryasir569@gmail.com>
2022-03-24 15:45:33 +0500

    templating web

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
-const https = require("https");
+const date = require(__dirname + "/date.js");
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const app = express();
-let items = ["Buy Food", "Cook Food"];
-let workItems = ["Design Post", "Design Slider"];
+const items = ["Buy Food", "Cook Food"];
+const workItems = ["Design Post", "Design Slider"];
 
 app.use("/public", express.static(process.cwd() + "/public"));
 app.set("view engine", "ejs");
@@ -17,19 +16,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    const day = date.getDate();
 
     res.render("lists", { listTitle: day, newListItems: items });
 });
 app.post("/", function (req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
@@ -43,11 +35,9 @@ app.post("/", function (req, res) {
 app.get("/work", function (req, res) {
     res.render("lists", { listTitle: "Work List", newListItems: workItems });
 });
-// app.post("/work", function (req, res) {
-//     let item = req.body.newItem;
-//     workItems.push(workItems);
-//     res.redirect("/work");
-// });
+app.get("/about", function (req, res) {
+    res.render("about");
+});
 
 app.listen(port, function () {
     console.log("Server is started at http://localhost:" + port);
